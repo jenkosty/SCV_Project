d55d8c183a297cdc9b7f560086e02289c5878dfc
jenkosty <82013074+jenkosty@users.noreply.github.com>
2022-10-27 21:05:07 -0700

    Add files via upload

--- LLC Scripts/LLC_Trial.m
+++ LLC Scripts/LLC_Trial.m
@@ -1,8 +1,8 @@
 clear; close all; clc
 
-%%% Loading seal data
+%%% Loading MEOP seal data
 load("qc_ts.mat")
-test_prof = 164;
+test_prof = 4;
 
 %%
 %%% Loading LLC data
@@ -15,122 +15,157 @@ LLC_5.mat = matfile('/Volumes/Elements/LLC_Snapshots/SO_snapshots_NSF_LLC4320_k1
 LLC_1.edge_lats = [double(LLC_1.mat.yc(1,:)), double(LLC_1.mat.yc(:,end))', flip(double(LLC_1.mat.yc(end,:))), flip(double(LLC_1.mat.yc(:,1)))'];
 LLC_1.edge_lons = [double(LLC_1.mat.xc(1,:)), double(LLC_1.mat.xc(:,end))', flip(double(LLC_1.mat.xc(end,:))), flip(double(LLC_1.mat.xc(:,1)))'];
 LLC_1.polygon = geopolyshape(LLC_1.edge_lats, LLC_1.edge_lons);
+LLC_1.lats = double(LLC_1.mat.yc);
+LLC_1.lons = double(LLC_1.mat.xc);
 
 LLC_2.edge_lats = [double(LLC_2.mat.yc(1,:)), double(LLC_2.mat.yc(:,end))', flip(double(LLC_2.mat.yc(end,:))), flip(double(LLC_2.mat.yc(:,1)))'];
 LLC_2.edge_lons = [double(LLC_2.mat.xc(1,:)), double(LLC_2.mat.xc(:,end))', flip(double(LLC_2.mat.xc(end,:))), flip(double(LLC_2.mat.xc(:,1)))'];
 LLC_2.polygon = geopolyshape(LLC_2.edge_lats, LLC_2.edge_lons);
+LLC_2.lats = double(LLC_2.mat.yc);
+LLC_2.lons = double(LLC_2.mat.xc);
 
 LLC_4.edge_lats = [double(LLC_4.mat.yc(1,:)), double(LLC_4.mat.yc(:,end))', flip(double(LLC_4.mat.yc(end,:))), flip(double(LLC_4.mat.yc(:,1)))'];
 LLC_4.edge_lons = [double(LLC_4.mat.xc(1,:)), double(LLC_4.mat.xc(:,end))', flip(double(LLC_4.mat.xc(end,:))), flip(double(LLC_4.mat.xc(:,1)))'];
 LLC_4.polygon = geopolyshape(LLC_4.edge_lats, LLC_4.edge_lons);
+LLC_4.lats = double(LLC_4.mat.yc);
+LLC_4.lons = double(LLC_4.mat.xc);
 
 LLC_5.edge_lats = [double(LLC_5.mat.yc(1,:)), double(LLC_5.mat.yc(:,end))', flip(double(LLC_5.mat.yc(end,:))), flip(double(LLC_5.mat.yc(:,1)))'];
 LLC_5.edge_lons = [double(LLC_5.mat.xc(1,:)), double(LLC_5.mat.xc(:,end))', flip(double(LLC_5.mat.xc(end,:))), flip(double(LLC_5.mat.xc(:,1)))'];
 LLC_5.polygon = geopolyshape(LLC_5.edge_lats, LLC_5.edge_lons);
+LLC_5.lats = double(LLC_5.mat.yc);
+LLC_5.lons = double(LLC_5.mat.xc);
+
+%%
+%%% Calculating the Okubo Weiss Parameter
+ind = [27 32];
+LLC_1.OW = Okubo_Weiss(LLC_1.mat.dxc(:,:), LLC_1.mat.dyc(:,:), LLC_1.mat.u(:,:,ind), LLC_1.mat.v(:,:,ind));
+LLC_2.OW = Okubo_Weiss(LLC_2.mat.dxc(:,:), LLC_2.mat.dyc(:,:), LLC_2.mat.u(:,:,ind), LLC_2.mat.v(:,:,ind));
+LLC_4.OW = Okubo_Weiss(LLC_4.mat.dxc(:,:), LLC_4.mat.dyc(:,:), LLC_4.mat.u(:,:,ind), LLC_4.mat.v(:,:,ind));
+LLC_5.OW = Okubo_Weiss(LLC_5.mat.dxc(:,:), LLC_5.mat.dyc(:,:), LLC_5.mat.u(:,:,ind), LLC_5.mat.v(:,:,ind));
 
 %%
 for tag_no = test_prof
-    for i = 1:length(qc_ts(tag_no).cast)
-        if isinterior(LLC_1.polygon, geopointshape(qc_ts(tag_no).lat(i), qc_ts(tag_no).lon(i)))
-            seal.sector(i) = 1;
-        elseif isinterior(LLC_2.polygon, geopointshape(qc_ts(tag_no).lat(i), qc_ts(tag_no).lon(i)))
-            seal.sector(i) = 2;
-        elseif isinterior(LLC_5.polygon, geopointshape(qc_ts(tag_no).lat(i), qc_ts(tag_no).lon(i)))
-            seal.sector(i) = 5;
-        elseif isinterior(LLC_4.polygon, geopointshape(qc_ts(tag_no).lat(i), qc_ts(tag_no).lon(i)))
-            seal.sector(i) = 4;
+    
+    clear LLCseal
+
+    %%% Grabbing data on MEOP seal time series
+    LLCseal.tag = qc_ts(tag_no).tag;
+    LLCseal.cast = qc_ts(tag_no).cast;
+    LLCseal.lat = qc_ts(tag_no).lat;
+    LLCseal.lon = qc_ts(tag_no).lon;
+    LLCseal.time = qc_ts(tag_no).time;
+
+    sector = NaN(1,length(LLCseal.cast));
+    %%% Identifying the LLC sector of each MEOP profile
+    for i = 1:length(LLCseal.cast)
+        if isinterior(LLC_1.polygon, geopointshape(LLCseal.lat(i), LLCseal.lon(i)))
+            sector(i) = 1;
+        elseif isinterior(LLC_2.polygon, geopointshape(LLCseal.lat(i), LLCseal.lon(i)))
+            sector(i) = 2;
+        elseif isinterior(LLC_5.polygon, geopointshape(LLCseal.lat(i), LLCseal.lon(i)))
+            sector(i) = 5;
+        else
+            sector(i) = 4;
         end
     end
-end
 
-%%
-for tag_no = test_prof
+    LLCseal.sector = sector;
+
+    %%% Initializing matrices for interpolated data
+    LLCseal_salt = NaN(size(qc_ts(tag_no).salt));
+    LLCseal_temp = NaN(size(qc_ts(tag_no).temp));
+    LLCseal_vort = NaN(size(qc_ts(tag_no).temp));
+    LLCseal_OW = NaN(size(LLC_1.OW, 3), length(qc_ts(tag_no).cast));
+
     for i = 1:length(qc_ts(tag_no).cast)
 
         %%% Grabbing LLC data for profile sector
-        if seal.sector(i) == 1
+        if sector(i) == 1
             LLC = LLC_1;
-        elseif seal.sector(i) == 2
+        elseif sector(i) == 2
             LLC = LLC_2;
-        elseif seal.sector(i) == 4
+        elseif sector(i) == 4
             LLC = LLC_4;
-        elseif seal.sector(i) == 5
+        elseif sector(i) == 5
             LLC = LLC_5;
         end
 
-        %%% Extracting indices of LLC data near profile lat/lon
-        dists = distance(qc_ts(tag_no).lat(i), qc_ts(tag_no).lon(i), double(LLC.mat.yc), double(LLC.mat.xc));
-        min_dists = mink(dists(:), 4);
-    
-        ind.row = NaN(1,4);
-        ind.col = NaN(1,4);
-        for j = 1:4
-            [ind.row(j), ind.col(j)] = find(dists == min_dists(j));
-        end
+        %%% Finding LLC points close to MEOP profile
+        LLC_lats = LLC.lats;
+        LLC_lons = LLC.lons;
+        nan_ind = LLC_lats > LLCseal.lat(i) + 0.025 | LLC_lats < LLCseal.lat(i) - 0.025 | LLC_lons > LLCseal.lon(i) + 0.025 | LLC_lons < LLCseal.lon(i) - 0.025;
+        LLC_lats(nan_ind) = NaN;
+        LLC_lons(nan_ind) = NaN;
+        close_ind = find(~isnan(LLC_lats));
+        [rows, cols] = ind2sub(size(LLC_lats), close_ind);
 
-        %%% Extracting LLC data
-        for j = 1:4
-            salt(:,j) = squeeze(LLC.mat.s(ind.row(j), ind.col(j), :));
-            temp(:,j) = squeeze(LLC.mat.t(ind.row(j), ind.col(j), :));
-            vort(:,j) = squeeze(LLC.mat.Ro(ind.row(j), ind.col(j), :));
+        lats_unfmt = NaN(1,length(rows));
+        lons_unfmt = NaN(1,length(rows));
+        salt = NaN(86, length(rows));
+        temp = NaN(86, length(rows));
+        vort = NaN(86, length(rows));
+        okubo_weiss = NaN(size(LLC.OW, 3), length(rows));
+
+        %%% Extracting LLC data close to MEOP profile
+        for j = 1:length(rows)
+            lats_unfmt(j) = double(LLC.mat.yc(rows(j), cols(j)));
+            lons_unfmt(j) = double(LLC.mat.xc(rows(j), cols(j)));
+            salt(:,j) = squeeze(LLC.mat.s(rows(j), cols(j), :));
+            temp(:,j) = squeeze(LLC.mat.t(rows(j), cols(j), :));
+            vort(:,j) = squeeze(LLC.mat.Ro(rows(j), cols(j), :));
+            okubo_weiss(:,j) = squeeze(LLC.OW(rows(j), cols(j),:));
         end
         temp(salt == 0) = NaN;
         vort(salt == 0) = NaN;
         salt(salt == 0) = NaN;
 
+        lats = double(lats_unfmt .* ones(size(salt)));
+        lons = double(lons_unfmt .* ones(size(salt)));
+        depths = LLC.mat.rc(1:size(salt, 1), 1 ) .* ones(size(salt));
+        
+        %%% Interpolating LLC data
+        S = scatteredInterpolant(lats(:), lons(:), depths(:), salt(:));
+        LLCseal_salt(:,i) = S(qc_ts(tag_no).lat(i).*ones(size(depth_grid)), qc_ts(tag_no).lon(i).*ones(size(depth_grid)), -depth_grid);
+
+        T = scatteredInterpolant(lats(:), lons(:), depths(:), temp(:));
+        LLCseal_temp(:,i) = T(qc_ts(tag_no).lat(i).*ones(size(depth_grid)), qc_ts(tag_no).lon(i).*ones(size(depth_grid)), -depth_grid);
+
+        V = scatteredInterpolant(lats(:), lons(:), depths(:), vort(:));
+        LLCseal_vort(:,i) = V(qc_ts(tag_no).lat(i).*ones(size(depth_grid)), qc_ts(tag_no).lon(i).*ones(size(depth_grid)), -depth_grid);
+
+        for j = 1:size(LLC.OW, 3)
+            OW = scatteredInterpolant(lats_unfmt', lons_unfmt', okubo_weiss(j,:)');
+            LLCseal_OW(j,i) = OW(qc_ts(tag_no).lat(i), qc_ts(tag_no).lon(i));
+        end
+
+        clear lats_unfmt lons_unfmt salt vort temp okubo_weiss cols rows nan_ind lats lons depths S T V OW j close_ind 
     end
+
+    LLCseal.salt = LLCseal_salt;
+    LLCseal.temp = LLCseal_temp;
+    LLCseal.vort = LLCseal_vort;
+    LLCseal.OW = LLCseal_OW;
+
+    %%% Saving interpolated LLC data to a structure
+    LLCsealdata(tag_no) = LLCseal;
+
+    clear sector i LLCseal LLCseal_salt LLCseal_temp LLCseal_vort LLCseal_OW LLC_lats LLC_lons
 end
 
 %%
-%%% Generating synthetic seal track
+%%% Using Okubo Weiss to flag profiles as eddies
 for tag_no = test_prof
-
-    %%% Cutting LLC Data by Latitude
-    [~,col_lat] = find(LLC.lat < max(qc_ts(tag_no).lat+0.1) & LLC.lat > min(qc_ts(tag_no).lat-0.1));
-    LON = LLC.lon(:,min(col_lat):max(col_lat));
-    LAT = LLC.lat(:,min(col_lat):max(col_lat));
-    SALT = LLC.salt(:,min(col_lat):max(col_lat),:);
-    TEMP = LLC.temp(:,min(col_lat):max(col_lat),:);
-    VORT = LLC.vort(:,min(col_lat):max(col_lat),:);
-
-    %%% Cutting LLC Data by Longitude
-    [row_lon,~] = find(LON < max(qc_ts(tag_no).lon+0.1) & LON > min(qc_ts(tag_no).lon-0.1));
-    LON_final = LON(min(row_lon):max(row_lon),:);
-    LAT_final = LAT(min(row_lon):max(row_lon),:);
-    SALT_final = SALT(min(row_lon):max(row_lon),:,:);
-    TEMP_final = TEMP(min(row_lon):max(row_lon),:,:);
-    VORT_final = VORT(min(row_lon):max(row_lon),:,:);
-    clear LON LAT SALT TEMP col_lat row_lon
-
-    %%% Formatting LLC Data for Interpolation
-    LON_final = LON_final .* ones(size(LON_final,1), size(LON_final,2), size(LLC.depth,1));
-    LAT_final = LAT_final .* ones(size(LAT_final,1), size(LAT_final,2), size(LLC.depth,1));
-    DEPTH(1,1,:) = -1 .* LLC.depth;
-    DEPTH_final = DEPTH .* ones(size(LAT_final,1), size(LAT_final,2), size(LLC.depth,1));
-    clear DEPTH
-
-    salt_interp = cell(size(qc_ts(tag_no).cast));
-    temp_interp = cell(size(qc_ts(tag_no).cast));
-    vort_interp = cell(size(qc_ts(tag_no).cast));
-    parfor i = 1:length(qc_ts(tag_no).cast)
-        salt_interp{:,i} = squeeze(interp3(LAT_final, LON_final, DEPTH_final, SALT_final,qc_ts(tag_no).lat(i), qc_ts(tag_no).lon(i),qc_ts(tag_no).raw_data(i).pres(~isnan(qc_ts(tag_no).raw_data(i).pres))));
-        temp_interp{:,i} = squeeze(interp3(LAT_final, LON_final, DEPTH_final, TEMP_final,qc_ts(tag_no).lat(i), qc_ts(tag_no).lon(i),qc_ts(tag_no).raw_data(i).pres(~isnan(qc_ts(tag_no).raw_data(i).pres))));
-        vort_interp{:,i} = squeeze(interp3(LAT_final, LON_final, DEPTH_final, VORT_final,qc_ts(tag_no).lat(i), qc_ts(tag_no).lon(i),qc_ts(tag_no).raw_data(i).pres(~isnan(qc_ts(tag_no).raw_data(i).pres))));
+    for i = 1:length(LLCsealdata(tag_no).cast)
+        if abs(LLCsealdata(tag_no).OW(1,i)) > 0.4e-8 && abs(LLCsealdata(tag_no).OW(2,i)) > 0.4e-8
+            LLCsealdata(tag_no).scv(i) = 1;
+        else
+            LLCsealdata(tag_no).scv(i) = 0;
+        end
     end
-
-    salt_unfmt = NaN(length(depth_grid), size(qc_ts(tag_no).cast,2));
-    temp_unfmt = NaN(length(depth_grid), size(qc_ts(tag_no).cast,2));
-    vort_unfmt = NaN(length(depth_grid), size(qc_ts(tag_no).cast,2));
-    for i = 1:length(qc_ts(tag_no).cast)
-        salt_unfmt(:,i) = interp1(qc_ts(tag_no).raw_data(i).pres(~isnan(qc_ts(tag_no).raw_data(i).pres)), salt_interp{:,i}, depth_grid);
-        temp_unfmt(:,i) = interp1(qc_ts(tag_no).raw_data(i).pres(~isnan(qc_ts(tag_no).raw_data(i).pres)), temp_interp{:,i}, depth_grid);
-        vort_unfmt(:,i) = interp1(qc_ts(tag_no).raw_data(i).pres(~isnan(qc_ts(tag_no).raw_data(i).pres)), vort_interp{:,i}, depth_grid);
-    end
-
-    LLC_data(tag_no) = struct("tag", qc_ts(tag_no).tag, "cast", qc_ts(tag_no).cast,...
-        "lat", qc_ts(tag_no).lat, "lon", qc_ts(tag_no).lon, "time", qc_ts(tag_no).time, "salt", salt_unfmt, "temp", temp_unfmt, ...
-        "vort", vort_unfmt);
 end
+
+
 
 %%
 
@@ -248,22 +283,26 @@ title('Rossby Number', 'FontSize', 12);
 
 
 %%
+
+%%%
+sea_ice = shaperead('/Users/jenkosty/Documents/Research/Sea_Ice_Extent/median_extent_S_12_1981-2010_polyline_v3.0/median_extent_S_12_1981-2010_polyline_v3.0');
+proj = shapeinfo('/Users/jenkosty/Documents/Research/Sea_Ice_Extent/median_extent_S_12_1981-2010_polyline_v3.0/median_extent_S_12_1981-2010_polyline_v3.0.shp').CoordinateReferenceSystem;
+[sea_ice_lat, sea_ice_lon] = projinv(proj, [sea_ice.X], [sea_ice.Y]);
+
 tag_no = 92;
-depth_ind = 30;
 %%% Plotting vorticity data
 load coastlines
-figure()
+figure('Position', [500 100 1000 850])
 axesm('stereo','Origin',[-90 0],'MapLatLimit',[-90 -57],'MLineLocation', 30, 'PLineLocation', 10, 'FontSize',10);
 axis off; framem on; gridm on; mlabel on; plabel on;
-colormap(cmocean('balance'));
-clim([-0.3 0.3]);
-colorbar;
+colormap(cmocean('balance')); colorbar; clim([-1e-8 1e-8]);
 hold on
-% pcolorm(LLC.lat(1:1:end,1:1:end), LLC.lon(1:1:end,1:1:end), LLC.vort(1:1:end,1:1:end,depth_ind));
+pcolorm(LLC_1.lats(:,:), LLC_1.lons(:,:), LLC_1.OW(:,:, 1));
+plotm(sea_ice_lat, sea_ice_lon, '-g')
 geoshow(coastlat, coastlon, 'DisplayType','polygon','FaceColor','white');
-plotm(LLC_4.edge_lats, LLC_4.edge_lons)
-plotm(qc_ts(tag_no).lat, qc_ts(tag_no).lon, 'g-','Marker', '.','MarkerSize', 10, 'LineWidth', 2)
-% plotm(qc_ts(tag_no).lat(154), qc_ts(tag_no).lon(154),'c', 'Marker', '*','MarkerSize', 10, 'LineWidth', 2)
+%plotm(qc_ts(tag_no).lat, qc_ts(tag_no).lon, 'g-','Marker', '.','MarkerSize', 10, 'LineWidth', 2)
 hold off
 %title('LLC Vorticity Data: ' + string(-LLC.depth(depth_ind)) + ' m')
+
+
 
